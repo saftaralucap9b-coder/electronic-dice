2089bd1c04e551572fdca5376cc4687d2511c661
Simion-Adelina <simion.maria.k6d@student.ucv.ro>
2026-04-16 11:54:22 +0300

    Create dice module skeleton
    Add initial structure for the electronic dice module. Define the basic initialization and update functions that will handle the dice logic, following a modular design.

---
+++ src/dice.c
@@ -0,0 +1,14 @@
+#include "dice.h"
+#include "pitches.h"
+#include "bsp/bsp.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/pwm/pwm.h"
+#include "drivers/timer/timer0.h"
+
+#include <stdlib.h>
+
+void Dice_Init(void) {
+}
+
+void Dice_Update(void) {
+}
