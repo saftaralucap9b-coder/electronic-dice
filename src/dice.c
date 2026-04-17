b38974b205cb22909a845d9330cbbaf1a4cd9fad
saftaralucap9b-coder <safta.raluca.p9b@student.ucv.ro>
2026-04-17 17:18:27 +0300

    Declare internal helper functions

--- src/dice.c
+++ src/dice.c
@@ -8,16 +8,16 @@
 #include <stdlib.h>
 
 typedef struct {
-    gpio_port_t port;
-    uint8_t pin;
+gpio_port_t port;
+uint8_t pin;
 } dice_pin_t;
-typedef enum {
-    DICE_STATE_IDLE,
+typedef enum {DICE_STATE_IDLE,
     DICE_STATE_BEEP_ON,
     DICE_STATE_BEEP_OFF,
     DICE_STATE_SHOW_RESULT
 } dice_state_t;
 static const dice_pin_t led_pins[6] = {{ D2 },{ D3 },{ D4 },{ D5 },{ D6 },{ D7 }};
+
 static dice_state_t dice_state = DICE_STATE_IDLE;
 
 static uint8_t dice_value = 0;
@@ -27,3 +27,10 @@ static gpio_state_t last_button_state = GPIO_HIGH;
 static uint32_t state_timestamp = 0;
 static uint32_t last_button_event = 0;
 static uint32_t entropy_counter = 0;
+
+static void Dice_Clear(void);
+static void Dice_Display(uint8_t value);
+static void Dice_StartBeep(uint16_t frequency);
+static void Dice_StopBeep(void);
+static uint8_t Dice_IsButtonPressed(void);
+static uint8_t Dice_GenerateRandomValue(void);
